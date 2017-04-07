cc210f5b40abb8a247ea3f01bab07373af9b464b
charly <charly@jean-michel.com>
2017-04-08 01:36:35 +0200

    [rosenbrock] tried to enhance generator

--- simulated_annealing/examples/rosenbrock.cpp
+++ simulated_annealing/examples/rosenbrock.cpp
@@ -11,15 +11,24 @@ struct Parameters
 
 int main()
 {
+    //     const long double pi = 128.546;
+    // std::cout << "default precision (6): " << pi << '\n'
+    //           << "std::setprecision(3): " << std::setprecision(3) << pi << '\t'
+    //           << "std::setprecision(10): " << std::setprecision(10) << pi << '\n'
+    //           << "max precision:         "
+    //           << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
+    //           << pi << '\n';
+    // return 0;
+
     std::random_device rd;
     std::mt19937 generator(rd());
     std::normal_distribution<> distrib(0, 1.0);
 
     SimulatedAnnealing sim(
-        1e3     //start temp
-        , 1e-3 //stop temp
-        , int(1e1) //max it
-        , 1e-4 //energy min
+          1e3      //start temp
+        , 1e-3     //stop temp
+        , int(1e6) //max it
+        , 1e-4     //energy min
         );
 
     auto ros_err = [](const Parameters& r)
@@ -27,11 +36,16 @@ int main()
         return (1 - r.x) * (1 - r.x) + 100 * (r.y - r.x * r.x) * (r.y - r.x * r.x);
     };
 
-    Parameters ros_parameters{100.0, 100.0};  
+    Parameters ros_parameters{100.0, 100.0};
 
-    auto ros_gen = [&](const Parameters& r)
+    auto ros_gen = [&](const Parameters& r, const SimulatedAnnealing& s)
     {
-        return Parameters{r.x + distrib(generator), r.y + distrib(generator)};
+        return Parameters{
+            //   r.x + 1.0 / (s.current_it != 0 ? s.current_it : 1.0) * distrib(generator)
+            // , r.y + 1.0 / (s.current_it != 0 ? s.current_it : 1.0) * distrib(generator)
+              r.x + distrib(generator)
+            , r.y + distrib(generator)
+        };
     };
 
     std::cout << "initial value: (" << ros_parameters.x << ", " << ros_parameters.y << ") (err: " << ros_err(ros_parameters) << ")" << std::endl;
