fd01302731f7d1c5b46aa0c13ec317835531cda2
Charles Antoine Noury <charles_antoine.noury@univ-bpclermont.fr>
2018-03-02 17:18:35 +0100

    [for_marie] created little tuto

---
+++ for_marie/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "truc.hpp"
+
+int main ()
+{
+    std::cout << "ni hao !" << std::endl;
+
+    A a;
+    a.function();
+
+    return 0;
+}
---
+++ for_marie/truc.cpp
@@ -0,0 +1,8 @@
+#include "truc.hpp"
+
+#include <iostream>
+
+void A::function()
+{
+    std::cout << "My name is A !" << std::endl;
+}
